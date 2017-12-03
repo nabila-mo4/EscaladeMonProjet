dd4de4921636419140c958b437db63ffbaf49db8
MOKADMI <nabila.mokadmi@capgemini.com>
2017-12-03 22:11:07 +0100

    modifier la servlet EnvoiEmailDemandeurPos

--- src/com/escalade/servlets/EnvoiEmailAEmprunteur.java
+++ src/com/escalade/servlets/EnvoiEmailAEmprunteur.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.escalade.beans.Site;
+
 import com.escalade.dao.DAOFactory;
 import com.escalade.dao.PretDao;
 
